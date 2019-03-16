92eca37f9f10bd506f0dc72234efbfea7d875e89
Marcel Schmittfull <mschmittfull@gmail.com>
2019-03-16 17:28:35 -0400

    fix binning check

--- interpolation_utils.py
+++ interpolation_utils.py
@@ -37,12 +37,14 @@ def interp1d_manual_k_binning(kin, Pin, kind='manual_Pk_k_bins', fill_value=None
         if k_bin_width == 1.:
             # 18 Jan 2019: somehow need 0.99 factor for nbodykit 0.3 to get last k bin right.
             kin_expected = np.arange(1,np.max(kin)*0.99/dk+1)*dk
+            # 16 Mar 2019: Fix expected k bins to match nbodykit for larger Ngrid
+            np.arange(1, kin.shape[0]+1)*dk
 
             if verbose:
                 print("kin:", kin)
                 print("kin_expected:", kin_expected)
                 print("kin/kin_expected (should be between 0.5 and 1.5):\n", kin/kin_expected)
-                
+
             # bin center is computed by averaging k within bin, so it's not exactly dk*i.
             if not np.allclose(kin, kin_expected, rtol=0.35):
                 print("kin:", kin)
