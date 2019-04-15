0499c262abbaf1d9fded9e425e432345b2167c96
Marcel Schmittfull <mschmittfull@gmail.com>
2019-04-15 17:22:02 -0400

    minor

--- ms_gadget_scripts/main_convert_fof_bigfile_to_hdf5.batch.sh
+++ ms_gadget_scripts/main_convert_fof_bigfile_to_hdf5.batch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -l
 
 set -x
+export MPICH_GNI_MBOXES_PER_BLOCK=4096
+
 
 MinMass=10.8
 MaxMass=11.8
@@ -10,7 +12,8 @@ source activate nbodykit-0.3.7-env
 
 myscript=main_convert_fof_bigfile_to_hdf5.py
 
-for SimSeed in 400 401 402 403 404
+#for SimSeed in 400 401 402 403 404
+for SimSeed in 400
 do
     python $myscript --SimSeed=$SimSeed --MinMass=$MinMass --MaxMass=$MaxMass --RSD=$RSD
 done
