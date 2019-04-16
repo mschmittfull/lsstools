609b87396620f53c9ddfa47273ee58312ea7d8b9
Marcel Schmittfull <mschmittfull@gmail.com>
2019-04-16 13:44:41 -0400

    minor

--- MeasuredPower.py
+++ MeasuredPower.py
@@ -1,8 +1,6 @@
 from __future__ import print_function,division
 from collections import namedtuple
 
-# simple named tuple to store output of power spectrum measurements
-#Pktuple = namedtuple('Pktuple', ['k', 'P', 'num_summands', 'info', 'info_id1', 'info_id2'])
 
 class MeasuredPower(object):
 	"""
