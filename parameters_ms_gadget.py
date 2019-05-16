9cd6bfe3a558c7315386d0ea10310cdfbf80cdd4
Marcel Schmittfull <mschmittfull@gmail.com>
2019-05-16 14:08:47 -0400

    refactor

--- parameters_ms_gadget.py
+++ parameters_ms_gadget.py
@@ -59,14 +59,18 @@ class MSGadgetSimOpts(SimOpts):
                                       include_shifted_fields=True,
                                       shifted_fields_RPsi=0.23,
                                       shifted_fields_Np=1536,
-                                      shifted_fields_Nmesh=1536):
+                                      shifted_fields_Nmesh=1536,
+                                      RSDstrings=None):
         """See parent class.
         """
         ext_grids = OrderedDict()
+        if RSDstrings is None:
+            RSDstrings = ['']
 
         if True:
-            # linear density (ICs of the sims)
-            # deltalin from mesh (created on mesh, no particles involved)
+            # Linear density (ICs of the sims).
+            # This is deltalin from mesh (created on mesh, without particles).
+            # This has no RSD.
             ext_grids['deltalin'] = {
                 'dir': 'IC_LinearMesh_z0_Ng%d' % Ngrid,
                 'file_format': 'nbkit_BigFileGrid',
@@ -92,7 +96,7 @@ class MSGadgetSimOpts(SimOpts):
         if False:
             # delta_ZA, created by moving 1536^3 ptcles with NGenic
             # (includes CIC artifacts, small shot noise)
-            ext_grids['deltaZA'] = {
+            ext_grids['deltaZA_NORSD'] = {
                 'dir':
                 'ZA_%.4f_PtcleDensity_Ng%d' %
                 (self.sim_scale_factor, Ngrid),
@@ -110,7 +114,8 @@ class MSGadgetSimOpts(SimOpts):
 
         if True:
             # deltanonl painted from all 1536^3 DM particles (includes CIC
-            # artifacts, small shot noise)
+            # artifacts, small shot noise).
+            # This has no RSD.
             ext_grids['delta_m'] = {
                 'dir':
                 'snap_%.4f_PtcleDensity_Ng%d' %
@@ -133,166 +138,164 @@ class MSGadgetSimOpts(SimOpts):
             #for psi_type_str in ['','Psi2LPT_']:
             psi_type_str = ''
 
-            # 1 shifted by deltalin_Zeldovich displacement (using nbkit0.3;
-            # same as delta_ZA)
-            ext_grids['1_SHIFTEDBY_%sdeltalin' % psi_type_str] = {
-                'dir':
-                '1_intR0.00_extR0.00_SHIFTEDBY_%sIC_LinearMeshR%.2f_a%.4f_Np%d_Nm%d_Ng%d_CICsum'
-                % (psi_type_str, shifted_fields_RPsi, self.sim_scale_factor,
-                   shifted_fields_Np, shifted_fields_Nmesh, Ngrid),
-                'file_format':
-                'nbkit_BigFileGrid',
-                'dataset_name':
-                'Field',
-                'scale_factor':
-                self.sim_scale_factor,
-                'nbkit_normalize':
-                True,
-                'nbkit_setMean':
-                0.0
-            }
+            for RSDstring in RSDstrings:
 
-            # deltalin shifted by deltalin_Zeldovich displacement (using 
-            # nbkit0.3)
-            ext_grids['deltalin_SHIFTEDBY_%sdeltalin' % psi_type_str] = {
-                'dir':
-                'IC_LinearMesh_intR0.00_extR0.00_SHIFTEDBY_%sIC_LinearMeshR%.2f_a%.4f_Np%d_Nm%d_Ng%d_CICsum'
-                % (psi_type_str, shifted_fields_RPsi, self.sim_scale_factor,
-                   shifted_fields_Np, shifted_fields_Nmesh, Ngrid),
-                'file_format':
-                'nbkit_BigFileGrid',
-                'dataset_name':
-                'Field',
-                'scale_factor':
-                self.sim_scale_factor,
-                'nbkit_normalize':
-                True,
-                'nbkit_setMean':
-                0.0
-            }
-
-            # deltalin^2 shifted by deltalin_Zeldovich displacement (using 
-            # nbkit0.3)
-            ext_grids[
-                'deltalin_growth-mean_SHIFTEDBY_%sdeltalin' % psi_type_str] = {
+                # 1 shifted by deltalin_Zeldovich displacement (using nbkit0.3;
+                # same as delta_ZA)
+                ext_grids['1_SHIFTEDBY_%sdeltalin%s' % (
+                    psi_type_str, RSDstring
+                )] = {
                     'dir':
-                    'IC_LinearMesh_growth-mean_intR0.00_extR0.00_SHIFTEDBY_%sIC_LinearMeshR%.2f_a%.4f_Np%d_Nm%d_Ng%d_CICsum'
+                    '1_intR0.00_extR0.00_SHIFTEDBY_%sIC_LinearMeshR%.2f_a%.4f_Np%d_Nm%d_Ng%d_CICsum%s'
                     % (psi_type_str, shifted_fields_RPsi, self.sim_scale_factor,
-                       shifted_fields_Np, shifted_fields_Nmesh, Ngrid),
-                    'file_format':
-                    'nbkit_BigFileGrid',
-                    'dataset_name':
-                    'Field',
-                    'scale_factor':
-                    self.sim_scale_factor,
-                    'nbkit_normalize':
-                    True,
-                    'nbkit_setMean':
-                    0.0
+                       shifted_fields_Np, shifted_fields_Nmesh, Ngrid,
+                       RSDstring),
+                    'file_format': 'nbkit_BigFileGrid',
+                    'dataset_name': 'Field',
+                    'scale_factor': self.sim_scale_factor,
+                    'nbkit_normalize': True,
+                    'nbkit_setMean': 0.0
                 }
 
-            # G2[deltalin] shifted by deltalin_Zeldovich displacement (using 
-            # nbkit0.3)
-            ext_grids['deltalin_G2_SHIFTEDBY_%sdeltalin' % psi_type_str] = {
-                'dir':
-                'IC_LinearMesh_tidal_G2_intR0.00_extR0.00_SHIFTEDBY_%sIC_LinearMeshR%.2f_a%.4f_Np%d_Nm%d_Ng%d_CICsum'
-                % (psi_type_str, shifted_fields_RPsi, self.sim_scale_factor,
-                   shifted_fields_Np, shifted_fields_Nmesh, Ngrid),
-                'file_format':
-                'nbkit_BigFileGrid',
-                'dataset_name':
-                'Field',
-                'scale_factor':
-                self.sim_scale_factor,
-                'nbkit_normalize':
-                True,
-                'nbkit_setMean':
-                0.0
-            }
+                # deltalin shifted by deltalin_Zeldovich displacement (using 
+                # nbkit0.3)
+                ext_grids['deltalin_SHIFTEDBY_%sdeltalin%s' % (
+                    psi_type_str, RSDstring
+                )] = {
+                    'dir':
+                    'IC_LinearMesh_intR0.00_extR0.00_SHIFTEDBY_%sIC_LinearMeshR%.2f_a%.4f_Np%d_Nm%d_Ng%d_CICsum%s'
+                    % (psi_type_str, shifted_fields_RPsi, self.sim_scale_factor,
+                       shifted_fields_Np, shifted_fields_Nmesh, Ngrid,
+                       RSDstring),
+                    'file_format': 'nbkit_BigFileGrid',
+                    'dataset_name': 'Field',
+                    'scale_factor': self.sim_scale_factor,
+                    'nbkit_normalize': True,
+                    'nbkit_setMean': 0.0
+                }
 
-            # deltalin^3 shifted by deltalin_Zeldovich displacement (using 
-            # nbkit0.3)
-            ext_grids['deltalin_cube-mean_SHIFTEDBY_%sdeltalin' % psi_type_str] = {
-                'dir':
-                'IC_LinearMesh_cube-mean_intR0.00_0.50_extR0.00_SHIFTEDBY_%sIC_LinearMeshR%.2f_a%.4f_Np%d_Nm%d_Ng%d_CICsum'
-                % (psi_type_str, shifted_fields_RPsi, self.sim_scale_factor,
-                   shifted_fields_Np, shifted_fields_Nmesh, Ngrid),
-                'file_format':
-                'nbkit_BigFileGrid',
-                'dataset_name':
-                'Field',
-                'scale_factor':
-                self.sim_scale_factor,
-                'nbkit_normalize':
-                True,
-                'nbkit_setMean':
-                0.0
-            }
+                # deltalin^2 shifted by deltalin_Zeldovich displacement (using 
+                # nbkit0.3)
+                ext_grids[
+                    'deltalin_growth-mean_SHIFTEDBY_%sdeltalin%s' % (
+                        psi_type_str, RSDstring
+                    )] = {
+                        'dir':
+                        'IC_LinearMesh_growth-mean_intR0.00_extR0.00_SHIFTEDBY_%sIC_LinearMeshR%.2f_a%.4f_Np%d_Nm%d_Ng%d_CICsum%s'
+                        % (psi_type_str, shifted_fields_RPsi, self.sim_scale_factor,
+                           shifted_fields_Np, shifted_fields_Nmesh, Ngrid,
+                           RSDstring),
+                        'file_format': 'nbkit_BigFileGrid',
+                        'dataset_name': 'Field',
+                        'scale_factor': self.sim_scale_factor,
+                        'nbkit_normalize': True,
+                        'nbkit_setMean': 0.0
+                    }
+
+                # G2[deltalin] shifted by deltalin_Zeldovich displacement (using 
+                # nbkit0.3)
+                ext_grids['deltalin_G2_SHIFTEDBY_%sdeltalin%s' % (
+                    psi_type_str, RSDstring)] = {
+                    'dir':
+                    'IC_LinearMesh_tidal_G2_intR0.00_extR0.00_SHIFTEDBY_%sIC_LinearMeshR%.2f_a%.4f_Np%d_Nm%d_Ng%d_CICsum%s'
+                    % (psi_type_str, shifted_fields_RPsi, self.sim_scale_factor,
+                       shifted_fields_Np, shifted_fields_Nmesh, Ngrid,
+                       RSDstring),
+                    'file_format': 'nbkit_BigFileGrid',
+                    'dataset_name': 'Field',
+                    'scale_factor': self.sim_scale_factor,
+                    'nbkit_normalize': True,
+                    'nbkit_setMean': 0.0
+                }
+
+                # deltalin^3 shifted by deltalin_Zeldovich displacement (using 
+                # nbkit0.3)
+                ext_grids['deltalin_cube-mean_SHIFTEDBY_%sdeltalin%s' % (
+                    psi_type_str, RSDstring)] = {
+                    'dir':
+                    'IC_LinearMesh_cube-mean_intR0.00_0.50_extR0.00_SHIFTEDBY_%sIC_LinearMeshR%.2f_a%.4f_Np%d_Nm%d_Ng%d_CICsum%s'
+                    % (psi_type_str, shifted_fields_RPsi, self.sim_scale_factor,
+                       shifted_fields_Np, shifted_fields_Nmesh, Ngrid,
+                       RSDstring),
+                    'file_format': 'nbkit_BigFileGrid',
+                    'dataset_name': 'Field',
+                    'scale_factor': self.sim_scale_factor,
+                    'nbkit_normalize': True,
+                    'nbkit_setMean': 0.0
+                }
 
         return ext_grids
 
-    def get_default_catalogs(self):
+    def get_default_catalogs(self, RSDstrings=None):
         """Default catalogs to load for ms_gadget sims.
         """
         cats = OrderedDict()
+        if RSDstrings is None:
+            RSDstrings = ['']
 
-        tmp_halo_dir = 'nbkit_fof_%.4f/ll_0.200_nmin25' % (
+        halo_dir = 'nbkit_fof_%.4f/ll_0.200_nmin25' % (
             self.sim_scale_factor)
 
-        ## nonuniform catalogs without ptcle masses
-        if True:
-            # halos without mass weight, narrow mass cuts: 10.8..11.8..12.8
-            # ..13.8..15.1
-            cats['delta_h'] = {
-                'in_fname':
-                "%s/fof_nbkfmt.hdf5_BOUNDS_log10M_%s.hdf5" %
-                (tmp_halo_dir, self.halo_mass_string),
-                'weight_ptcles_by':
-                None
-            }
+        for RSDstring in RSDstrings:
+            if RSDstring == '':
+                RSDfilestr = ''
+            else:
+                RSDfilestr = '%s.hdf5' % RSDstring
 
-        if False:
-            # halos not weighted by mass but including mass info in file,
-            # broad mass cut
-            # TODO: looks like nbodykit 0.3 does not read this properly b/c
-            # of hdf5. Should switch all files to bigfile at some point.
-            cats['delta_h'] = {
-                'in_fname':
-                "%s/fof_nbkfmt.hdf5_WithMassCols.hdf5_BOUNDS_log10M_%s.hdf5" %
-                (tmp_halo_dir, self.halo_mass_string),
-                'weight_ptcles_by':
-                None
-            }
+            ## nonuniform catalogs without ptcle masses
+            if True:
+                # halos without mass weight, narrow mass cuts: 10.8..11.8..12.8
+                # ..13.8..15.1
+                cats['delta_h%s' % RSDstring] = {
+                    'in_fname':
+                    "%s/fof_nbkfmt.hdf5_BOUNDS_log10M_%s.hdf5%s" %
+                    (halo_dir, self.halo_mass_string, RSDfilestr),
+                    'weight_ptcles_by':
+                    None
+                }
 
-        if False:
-            # halos in narrow mass bins, no mass weights
-            cats['delta_h_M10.8-11.8'] = {
-                'in_fname':
-                "%s/fof_nbkfmt.hdf5_WithMassCols_BOUNDS_log10M_10.8_11.8.hdf5" %
-                tmp_halo_dir,
-                'weight_ptcles_by':
-                None
-            }
-            cats['delta_h_M11.8-12.8'] = {
-                'in_fname':
-                "%s/fof_nbkfmt.hdf5_WithMassCols_BOUNDS_log10M_11.8_12.8.hdf5" %
-                tmp_halo_dir,
-                'weight_ptcles_by':
-                None
-            }
-            cats['delta_h_M12.8-13.8'] = {
-                'in_fname':
-                "%s/fof_nbkfmt.hdf5_WithMassCols_BOUNDS_log10M_12.8_13.8.hdf5" %
-                tmp_halo_dir,
-                'weight_ptcles_by':
-                None
-            }
-            cats['delta_h_M13.8-15.1'] = {
-                'in_fname':
-                "%s/fof_nbkfmt.hdf5_WithMassCols_BOUNDS_log10M_13.8_15.1.hdf5" %
-                tmp_halo_dir,
-                'weight_ptcles_by':
-                None
-            }
+            if False:
+                # halos not weighted by mass but including mass info in file,
+                # broad mass cut
+                # TODO: looks like nbodykit 0.3 does not read this properly b/c
+                # of hdf5. Should switch all files to bigfile at some point.
+                cats['delta_h%s' % RSDstring] = {
+                    'in_fname':
+                    "%s/fof_nbkfmt.hdf5_WithMassCols.hdf5_BOUNDS_log10M_%s.hdf5%s" %
+                    (halo_dir, self.halo_mass_string, RSDfilestr),
+                    'weight_ptcles_by':
+                    None
+                }
+
+            if False:
+                # halos in narrow mass bins, no mass weights
+                cats['delta_h_M10.8-11.8%s' % RSDstring] = {
+                    'in_fname':
+                    "%s/fof_nbkfmt.hdf5_WithMassCols_BOUNDS_log10M_10.8_11.8.hdf5%s" %
+                    (halo_dir, RSDfilestr),
+                    'weight_ptcles_by':
+                    None
+                }
+                cats['delta_h_M11.8-12.8%s' % RSDstring] = {
+                    'in_fname':
+                    "%s/fof_nbkfmt.hdf5_WithMassCols_BOUNDS_log10M_11.8_12.8.hdf5%s" %
+                    (halo_dir, RSDfilestr),
+                    'weight_ptcles_by':
+                    None
+                }
+                cats['delta_h_M12.8-13.8%s' % RSDstring] = {
+                    'in_fname':
+                    "%s/fof_nbkfmt.hdf5_WithMassCols_BOUNDS_log10M_12.8_13.8.hdf5%s" %
+                    (halo_dir, RSDfilestr),
+                    'weight_ptcles_by':
+                    None
+                }
+                cats['delta_h_M13.8-15.1%s' % RSDstring] = {
+                    'in_fname':
+                    "%s/fof_nbkfmt.hdf5_WithMassCols_BOUNDS_log10M_13.8_15.1.hdf5%s" %
+                    (halo_dir, RSDfilestr),
+                    'weight_ptcles_by':
+                    None
+                }
 
         return cats
